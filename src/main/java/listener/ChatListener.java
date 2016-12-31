0be665d19913a334d97abcb8c8e6320296974ace
Blogshot <knottsascha@gmail.com>
2016-12-31 01:30:35 +0100

    Reworked arguments/parameters according to IEEE

--- src/main/java/listener/ChatListener.java
+++ src/main/java/listener/ChatListener.java
@@ -19,7 +19,7 @@ import java.util.List;
 import java.util.Random;
 
 public class ChatListener implements IListener<MessageReceivedEvent> {
-
+  
   private final String helptext =
       "Trump-Bot usage:\n```\n"
           + "!trump  \t[options]\n"
@@ -27,63 +27,62 @@ public class ChatListener implements IListener<MessageReceivedEvent> {
           + "!merkel \t[options]\n"
           + "\nOptions:\n\n"
           + "  -help, -h      \tShow this message\n"
-          + "  -c:<channel>   \tSpecify voice channel to join\n"
-          + "  -f:<pattern>   \tSpecify sound file to play. Wildcard: *\n"
+          + "  -c <channel>   \tSpecify voice channel to join\n"
+          + "  -f <pattern>   \tSpecify sound file to play. Wildcard: *\n"
           + "  -sounds        \tList all available sound files\n"
           + "  -stats         \tPrint a short summary of statistics\n"
           + "  -leave         \tForce-leave the current channel\n"
-          + "  -contact:<text>\tContact the creator (bugs, feedback, etc)```";
-
+          + "  -contact <text>\tContact the creator (bugs, feedback, etc)```";
+  
   private final String helptext_admin =
       "Trump-Bot admin usage:\n```\n"
-          + "!trump  \t[options]\n"
+          + "!trump-admin\t[options]\n"
           + "\nOptions:\n\n"
           + "  -h, --help                         \tShow this message\n"
           + "  --list-tickets                     \tList open tickets\n"
           + "  --close-ticket <ticketID>          \tClose ticket\n"
           + "  --reply-ticket <ticketID> <message>\tReply to ticket```";
-
+  
   @Override
   public void handle(MessageReceivedEvent event) {
-
+    
     // get message content
     String message = event.getMessage().getContent().toLowerCase();
     IUser user = event.getMessage().getAuthor();
-
+    
     // support-console
-    if (message.startsWith("!trump -admin") && user.getID().equals(Main.getInstance().adminID)) {
-      System.out.println("Admin console invoked!");
+    if (message.startsWith("!trump-admin") && user.getID().equals(Main.getInstance().adminID)) {
       handleAdmin(event);
       return;
     }
-
+    
     Main.Politician politician = null;
-
+    
     // set politician
     for (Main.Politician value : Main.Politician.values()) {
       if (message.startsWith("!" + value.name())) {
         politician = value;
       }
     }
-
+    
     if (politician != null) {
       handleDefault(event, politician);
     }
   }
-
+  
   private void handleAdmin(MessageReceivedEvent event) {
-
+    
     // get message content
     String message = event.getMessage().getContent().toLowerCase();
-
-    message = message.replace("!trump -admin ", "");
-
+    
+    message = message.replace("!trump-admin ", "");
+    
     boolean hasArguments = !message.equals("");
-
+    
     if (hasArguments) {
-
-      for (String argument : getAdminArguments(message)) {
-
+      
+      for (String argument : getArguments(message)) {
+        
         if (argument.equals("--list-tickets")) {
           new ErrorReporter(event.getClient())
               .report(Main.getInstance().supportRequests.createReport());
@@ -92,11 +91,11 @@ public class ChatListener implements IListener<MessageReceivedEvent> {
           String value = argument.replace("--close-ticket ", "");
           
           String[] ticket_ids = value.split(" ");
-
+          
           for (String ticket_id : ticket_ids) {
             try {
               int id = Integer.parseInt(ticket_id);
-    
+              
               Main.getInstance().supportRequests.removeID(id);
             } catch (NumberFormatException e) {
               new ErrorReporter(event.getClient()).report(e);
@@ -104,17 +103,17 @@ public class ChatListener implements IListener<MessageReceivedEvent> {
             }
           }
           Main.getInstance().saveSupportRequests();
-
+          
         } else if (argument.startsWith("--reply-ticket ")) {
           String value = argument.replace("--reply-ticket ", "");
-
+          
           try {
             String ticket_id = value.substring(0, value.indexOf(" "));
             String reply = value.substring(value.indexOf(" ") + 1);
-
+            
             SupportRequest request =
                 Main.getInstance().supportRequests.getFromID(Integer.parseInt(ticket_id));
-
+            
             if (request != null) {
               request.reply(reply, event.getClient(), true);
             } else {
@@ -125,28 +124,27 @@ public class ChatListener implements IListener<MessageReceivedEvent> {
           } catch (StringIndexOutOfBoundsException e) {
             Main.getInstance().writeMessage(event.getMessage().getChannel(), "Invalid ticket-id.");
           }
-
+          
         } else {
           printAdminHelp(event.getMessage().getChannel());
         }
       }
     }
   }
-
-  private String[] getAdminArguments(String message) {
-    return message.replace(" --", " ----").split(" --");
+  
+  private String[] getArguments(String message) {
+    return message.replace(" -", " --").split(" -");
   }
-
+  
   private void handleDefault(MessageReceivedEvent event, Main.Politician politician) {
-
+    
     // get message content
     String message = event.getMessage().getContent().toLowerCase();
     IUser user = event.getMessage().getAuthor();
-
-    // trim string
-    message = message.trim();
-
-    boolean hasArguments = message.length() > ("!" + politician.name()).length();
+  
+    message = message.replace("!" + politician.name() + " ", "");
+  
+    boolean hasArguments = !message.equals("");
 
     /*
        INITIALISATION
@@ -156,17 +154,17 @@ public class ChatListener implements IListener<MessageReceivedEvent> {
        - sound-file to play (requires politician)
        - voice-channel to connect to
     */
-
+    
     // init text channel
     IChannel textChannel = event.getMessage().getChannel();
-
+    
     // init sound with random
     ArrayList<URL> soundFiles = new ArrayList<>();
-
+    
     // init voice channel with author's
     IVoiceChannel voiceChannel = null;
     List<IVoiceChannel> voiceChannels = user.getConnectedVoiceChannels();
-
+    
     if (voiceChannels.size() > 0) {
       voiceChannel = voiceChannels.get(0);
     }
@@ -174,14 +172,14 @@ public class ChatListener implements IListener<MessageReceivedEvent> {
     /*
        HANDLE PARAMETERS
     */
-
+    
     // has parameters
     if (hasArguments) {
-
-      for (String argument : getArguments(message, event)) {
-
+      
+      for (String argument : getArguments(message)) {
+        
         // help-message
-        if (argument.equals("-help") || argument.equals("-h")) {
+        if (argument.equals("--help") || argument.equals("-h")) {
 
           /*
           print help and exit
@@ -192,16 +190,16 @@ public class ChatListener implements IListener<MessageReceivedEvent> {
           /*
           custom channel
            */
-        } else if (argument.startsWith("-c:")) {
-
-          String value = argument.substring(argument.indexOf("-c:") + 3);
+        } else if (argument.startsWith("--channel ") || argument.startsWith("-c ")) {
+          
+          String value = argument.substring(argument.indexOf(" ") + 1);
           boolean found = false;
-
+          
           IGuild guild = event.getMessage().getGuild();
-
+          
           // iterate through available channels to find the specified channel
           for (IVoiceChannel candidate : guild.getVoiceChannels()) {
-
+            
             // set voicechannel
             if (candidate.getName().toLowerCase().equals(value)) {
               voiceChannel = candidate;
@@ -213,7 +211,7 @@ public class ChatListener implements IListener<MessageReceivedEvent> {
           if no channel was found
            */
           if (!found) {
-
+            
             // invalid channel, report and exit
             Main.getInstance()
                 .writeMessage(
@@ -226,38 +224,38 @@ public class ChatListener implements IListener<MessageReceivedEvent> {
           /*
           custom sound file
            */
-        } else if (argument.startsWith("-f:")) {
-
-          String value = argument.substring(argument.indexOf("-f:") + 3);
-
+        } else if (argument.startsWith("--file ") || argument.startsWith("-f ")) {
+          
+          String value = argument.substring(argument.indexOf(" ") + 1);
+          
           // make * as wildcard work
           String pattern = ("\\Q" + value + "\\E").replace("*", "\\E.*\\Q");
-
+          
           // get list of matching files
           ArrayList<URL> candidates = getAudio(politician, pattern);
-
+          
           if (candidates.size() == 0) {
-
+            
             // no match found, cant continue. report and exit
             Main.getInstance()
                 .writeMessage(
                     textChannel, "I could not find a filename matching the pattern you specified.");
             return;
-
+            
           } else if (candidates.size() > 1) {
-
+            
             // multiple matches
             String matches = fileListToString(candidates);
-
+            
             Main.getInstance()
                 .writeMessage(
                     textChannel,
                     "I found multiple audios matching your pattern. Please select one of the following:\n\n"
                         + matches);
             return;
-
+            
           } else {
-
+            
             // set the only match as desired audio
             soundFiles.add(candidates.get(0));
           }
@@ -265,14 +263,14 @@ public class ChatListener implements IListener<MessageReceivedEvent> {
           /*
            list all sounds
           */
-        } else if (argument.equals("-sounds")) {
-
+        } else if (argument.equals("--sounds")) {
+          
           File audio = new File("audio/" + politician.name());
-
+          
           File[] files = audio.listFiles();
-
+          
           String matches = fileListToString(files);
-
+          
           Main.getInstance()
               .writeMessage(textChannel, "Following files are available:\n\n" + matches);
           return;
@@ -280,33 +278,33 @@ public class ChatListener implements IListener<MessageReceivedEvent> {
           /*
            print stats to channel
           */
-        } else if (argument.equals("-stats")) {
+        } else if (argument.equals("-stats") || argument.equals("-statistics")) {
           printStats(textChannel);
           return;
-        } else if (argument.startsWith("-contact:")) {
-          String value = argument.substring(argument.indexOf("-contact:") + 9);
-
+        } else if (argument.startsWith("--report ") || argument.equals("-r ")) {
+          String value = argument.substring(argument.indexOf(" ") + 1);
+          
           SupportRequest request = new SupportRequest(user.getID(), value);
-
+          
           Main.getInstance().supportRequests.add(request);
-
+          
           Main.getInstance().saveSupportRequests();
-
+          
           try {
             IPrivateChannel privateChannel = user.getOrCreatePMChannel();
-
+            
             Main.getInstance()
                 .writeMessage(
                     privateChannel,
                     "You have successfully opened ticket #"
                         + request.ticket_id
-                        + ".\nYou can add comments to it by typing '!trump -ticket:"
+                        + ".\nYou can add comments to it by typing '!trump -ticket "
                         + request.ticket_id
-                        + " <your text here>'");
+                        + " <message>'");
           } catch (DiscordException | RateLimitException e) {
             e.printStackTrace();
           }
-
+          
           new ErrorReporter(event.getClient())
               .report(
                   "A new support-request was added:\n"
@@ -315,38 +313,39 @@ public class ChatListener implements IListener<MessageReceivedEvent> {
                       + ":\n"
                       + request.message
                       + "\n```");
-
+          
           return;
-        } else if (argument.startsWith("-ticket:")) {
-
-          String value = argument.replace("-ticket:", "");
-
-          try {
-            String ticket_id = value.substring(0, value.indexOf(" "));
-            String reply = value.substring(value.indexOf(" ") + 1);
-
-            SupportRequest request =
-                Main.getInstance().supportRequests.getFromID(Integer.parseInt(ticket_id));
-
-            if (request == null) {
-              Main.getInstance()
-                  .writeMessage(event.getMessage().getChannel(), "Invalid ticket-id.");
-              return;
-            }
-  
-            if (!request.user_id.equals(user.getID())) {
-              Main.getInstance()
-                  .writeMessage(event.getMessage().getChannel(), "You have no permission to reply to this ticket.");
-              return;
-            }
-            
-            request.reply(reply, event.getClient(), false);
-            
-          } catch (Exception e) {
+        } else if (argument.startsWith("--ticket ") || argument.startsWith("-t ")) {
+          
+          String value = argument.substring(argument.indexOf(" ") + 1);
+          
+          // two arguments given (ID and MESSAGE)
+          if (!value.contains(" ")) {
             Main.getInstance()
-                .writeMessage(
-                    event.getMessage().getChannel(), "No parameter for content detected.");
+                .writeMessage(event.getMessage().getChannel(), "You must specify <ticket-id> and <message>, in that order.");
+            return;
           }
+          
+          String ticket_id = value.substring(0, value.indexOf(" "));
+          String reply = value.substring(value.indexOf(" ") + 1);
+          
+          SupportRequest request =
+              Main.getInstance().supportRequests.getFromID(Integer.parseInt(ticket_id));
+          
+          if (request == null) {
+            Main.getInstance()
+                .writeMessage(event.getMessage().getChannel(), "Invalid ticket-id.");
+            return;
+          }
+          
+          if (!request.user_id.equals(user.getID())) {
+            Main.getInstance()
+                .writeMessage(event.getMessage().getChannel(), "You have no permission to reply to this ticket.");
+            return;
+          }
+          
+          request.reply(reply, event.getClient(), false);
+          
           return;
         } else if (argument.startsWith("-support")) {
           new ErrorReporter(event.getClient())
@@ -361,10 +360,10 @@ public class ChatListener implements IListener<MessageReceivedEvent> {
         }
       }
     }
-
+    
     // Abort if bot is busy
     // located here to allow parameters like '-stats' or '-help' to be displayed while bot is active
-
+    
     IVoiceChannel usedChannel = Main.getInstance().isBusyInGuild(event.getMessage().getGuild());
     if (usedChannel != null) {
       Main.getInstance()
@@ -372,7 +371,7 @@ public class ChatListener implements IListener<MessageReceivedEvent> {
               textChannel, "I am currently needed in Channel '" + usedChannel.getName() + "'.");
       return;
     }
-
+    
     if (voiceChannel == null) {
       Main.getInstance()
           .writeMessage(
@@ -380,19 +379,19 @@ public class ChatListener implements IListener<MessageReceivedEvent> {
               "Look, you have to be in a voicechannel (or specify one by adding '-c:<name of channel>' to do this.");
       return;
     }
-
+    
     if (soundFiles.isEmpty()) {
       soundFiles.add(getRandomAudio(politician));
     }
-
+    
     Main.getInstance().playAudio(voiceChannel, textChannel, soundFiles, user);
   }
-
+  
   private void printStats(IChannel textChannel) {
-
+    
     SimpleDateFormat sdf = new SimpleDateFormat("MMM dd,yyyy HH:mm");
     Date startDate = new Date(Main.getInstance().startedInMillis);
-
+    
     String output =
         "Current stats:\n"
             + "```"
@@ -408,102 +407,69 @@ public class ChatListener implements IListener<MessageReceivedEvent> {
             + "Currently active guilds:  \t"
             + readStat("guildCount")
             + "```";
-
+    
     Main.getInstance().writeMessage(textChannel, output);
   }
-
+  
   private String readStat(String stat) {
-
+    
     return Main.getInstance().getStatsAsJson().get(stat).getAsString();
   }
-
+  
   private String fileListToString(File[] files) {
     // multiple matches
     String matches = "";
     for (File file : files) {
       matches += file.getName() + "\n";
     }
-
+    
     return matches.trim();
   }
-
+  
   private String fileListToString(ArrayList<URL> files) {
     // multiple matches
     String matches = "";
     for (URL file : files) {
       matches += file + "\n";
     }
-
+    
     return matches.trim();
   }
-
+  
   private void printAdminHelp(IChannel textChannel) {
     Main.getInstance().writeMessage(textChannel, helptext_admin);
   }
-
+  
   private void printHelp(String intro, IChannel textChannel) {
     Main.getInstance().writeMessage(textChannel, intro + "\n\n" + helptext);
   }
-
+  
   private void printHelp(IChannel textChannel) {
     Main.getInstance().writeMessage(textChannel, helptext);
   }
-
+  
   private String getVoiceChannelList(IGuild guild) {
-
+    
     String list = "";
-
+    
     for (IVoiceChannel channel : guild.getVoiceChannels()) {
       list += channel.getName() + "\n";
     }
-
+    
     return list;
   }
-
-  private ArrayList<String> getArguments(String message, MessageReceivedEvent event) {
-    /*
-     get argument-string
-     !trump /c:test -f=china
-     ->
-     -c=test -f=china
-
-    */
-
-    ArrayList<String> args = new ArrayList<>();
-
-    int mark = message.length();
-
-    for (int i = message.length() - 1; i >= 0; i--) {
-
-      try {
-
-        // if there is " -"
-        if (message.charAt(i) == '-' && message.charAt(i - 1) == ' ') {
-          args.add(message.substring(i, mark));
-
-          // set mark at space
-          mark = i - 1;
-        }
-
-      } catch (IndexOutOfBoundsException e) {
-        new ErrorReporter(event.getClient()).report(e);
-      }
-    }
-
-    return args;
-  }
-
+  
   private ArrayList<URL> getAudio(Main.Politician politician, String pattern) {
-
+    
     File audio = new File("audio/" + politician.name());
-
+    
     File[] files = audio.listFiles();
     ArrayList<URL> candidates = new ArrayList<>();
-
+    
     // iterate through available files to find matching ones
     if (files != null) {
       for (File candidate : files) {
-
+        
         // get matches
         if (candidate.getName().matches(pattern)) {
           try {
@@ -514,30 +480,30 @@ public class ChatListener implements IListener<MessageReceivedEvent> {
         }
       }
     }
-
+    
     return candidates;
   }
-
+  
   private URL getRandomAudio(Main.Politician politician) {
-
+    
     // set path for selected politician
     File audio = new File("audio/" + politician.name());
-
+    
     URL soundFile = null;
-
+    
     // pick a random audio
     File[] files = audio.listFiles();
-
+    
     if (files != null && files.length > 0) {
       int random = new Random().nextInt(files.length);
-
+      
       try {
         soundFile = files[random].toURI().toURL();
       } catch (MalformedURLException e) {
         e.printStackTrace();
       }
     }
-
+    
     return soundFile;
   }
 }
