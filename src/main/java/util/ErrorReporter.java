529d7860863ba6e0eac2e96873f189b1c19c89de
Blogshot <knottsascha@gmail.com>
2017-01-13 14:36:28 +0100

    implemented static bug-channel without searching every guild

--- src/main/java/util/ErrorReporter.java
+++ src/main/java/util/ErrorReporter.java
@@ -1,7 +1,8 @@
 package util;
 
-import main.Main;
 import sx.blah.discord.api.IDiscordClient;
+import sx.blah.discord.handle.obj.IChannel;
+import sx.blah.discord.handle.obj.IGuild;
 import sx.blah.discord.util.MessageBuilder;
 
 import java.io.PrintWriter;
@@ -10,9 +11,15 @@ import java.io.StringWriter;
 public class ErrorReporter {
 
   private final IDiscordClient client;
-
+  private IChannel bugChannel;
+  
   public ErrorReporter(IDiscordClient client) {
+    
     this.client = client;
+  
+    // get bug-channel
+    IGuild guild = client.getGuildByID("269206577418993664");
+    this.bugChannel = guild.getChannelByID("269364663349805057");
   }
 
   public void report(Exception e) {
@@ -23,7 +30,7 @@ public class ErrorReporter {
 
     try {
       new MessageBuilder(client)
-          .withChannel(Main.getInstance().bugChannel)
+          .withChannel(bugChannel)
           .withContent(message)
           .build();
     } catch (Exception e) {
