eb0f7722582087895d1a795198d97567fc4fbf51
Blogshot <knottsascha@gmail.com>
2017-01-14 20:48:39 +0100

    Removed delay, did not do anything

--- src/main/java/listener/TrackFinishedListener.java
+++ src/main/java/listener/TrackFinishedListener.java
@@ -9,22 +9,13 @@ public class TrackFinishedListener implements IListener<TrackFinishEvent> {
   @Override
   public void handle(TrackFinishEvent event) {
 
-    /*
-     Wait a few ms to fix 'java.net.SocketException: Socket is closed'.
-     (Audio-packet length is 20ms, use 21ms)
-      */
-    try {
-      Thread.sleep(21);
-    } catch (InterruptedException ignored) {
-    }
-
     // Leave current channel after audio finished
     Main.getInstance().leaveVoiceChannel(event.getPlayer().getGuild());
 
     // Clean memory to countermeasure memory leak
     // https://github.com/austinv11/Discord4J/issues/191)
     event.getPlayer().clean();
-  
+    
     Main.log("Left voice channel and cleaned guild's player.");
 
     // Update stats
