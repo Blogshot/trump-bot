331b2590599a350e9d3944aca66f6b40a6da2a43
Blogshot <knottsascha@gmail.com>
2017-01-14 20:47:19 +0100

    Added examples to help-message

--- src/main/java/listener/ChatListener.java
+++ src/main/java/listener/ChatListener.java
@@ -20,17 +20,21 @@ public class ChatListener implements IListener<MessageReceivedEvent> {
   
   private final String helptext =
       "Trump-Bot usage:\n```\n"
-          + "!trump  \t[options]\n"
-          + "!clinton\t[options]\n"
-          + "!merkel \t[options]\n"
+          + "!trump   [options]\n"
+          + "!clinton [options]\n"
+          + "!merkel  [options]\n"
           + "\nOptions:\n\n"
           + "  -h, --help  \tShow this message\n"
           + "  -c <channel>\tSpecify voice channel to join\n"
           + "  -f <pattern>\tSpecify sound file to play. Wildcard: *\n"
           + "  --sounds    \tList all available sound files\n"
           + "  --stats     \tPrint a short summary of statistics\n"
-          + "  --leave     \tForce-leave the current channel\n```\n"
-          + "Keeping the servers running costs money. Please consider donating. Visit https://trump.knotti.org for more info.\n\n"
+          + "  --leave     \tForce-leave the current channel\n\n"
+          + "Examples:\n"
+          + "!trump -f big-china.mp3 -c General\n"
+          + "!trump -f big-*.mp3 -c General\n"
+          + "```\n"
+          + "Keeping the server running costs money, please consider donating. Visit https://trump.knotti.org for more info.\n\n"
           + "If you need assistance or want to share feedback, contact Bloggi#7559 or join the support-discord: https://discord.gg/MzfyfTm"
       ;
   
