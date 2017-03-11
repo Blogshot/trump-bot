b0e92be777fe6b2c6cbaeed90abc63b9f1366611
Blogshot <knottsascha@gmail.com>
2017-03-11 12:23:03 +0100

    Added counter for shards to stats

--- trump-bot.js
+++ trump-bot.js
@@ -238,6 +238,7 @@ function writeStats() {
         file.trump = trump;
         file.clinton = clinton;
         file.merkel = merkel;
+        file.shards = results.length;
 
         fs.writeFile(
             fileName,
