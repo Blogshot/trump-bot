fc527ed68dd7f48d42dc670bc60603f6caf08930
Blogshot <15980143+Blogshot@users.noreply.github.com>
2022-08-27 00:47:59 +0200

    Add stderr as output

--- util/logger.js
+++ util/logger.js
@@ -10,5 +10,17 @@ module.exports = {
             "[" + date.toLocaleDateString() + " " + date.toLocaleTimeString() + "] " +
             shard_string + " " + JSON.stringify(message, null, 2)
         );
+    },
+
+    err: function (shard, message) {
+        var date = new Date();
+
+        var shard_string = (shard == null) ? "" : " ==== " + shard;
+
+        // "[30.1.2017 10:44:19] - Here is your message."
+        console.error(
+            "[" + date.toLocaleDateString() + " " + date.toLocaleTimeString() + "] " +
+            shard_string + " " + JSON.stringify(message, null, 2)
+        );
     }
 }
